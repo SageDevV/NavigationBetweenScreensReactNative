67f4f137a5fab512b05b0edec6d0da16c9a63474
SageDevV <pandredbz@gmail.com>
2022-09-02 04:16:21 -0300

    first commit

---
+++ src/telas/Produtor/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useRoute } from '@react-navigation/native';
+import useTexto from '../../hooks/useTextos';
+
+import { FlatList, StyleSheet, View, Image, Text } from 'react-native';
+import Cesta from './componentes/Cesta';
+import Topo from '../../componentes/Topo';
+
+import topo from '../../assets/produtores/topo.png'
+
+
+
+export default function Produtor() {
+    const route = useRoute();
+    const { nome, imagem, cestas } = route.params;
+    const { tituloProdutor, tituloCesta } = useTexto();
+
+    const listaTopo = () => {
+        return <>
+            <Topo titulo={tituloProdutor} imagem={topo} altura={150} />
+            <View style={estilos.conteudo}>
+                <View style={estilos.logo}>
+                    <Image source={imagem} style={estilos.produtorImage}/>
+                    <Text style={estilos.produtor}>{nome}</Text>
+                </View>
+            </View>
+        </>
+    }
+
+    return < FlatList
+        ListHeaderComponent={listaTopo}
+        data={cestas}
+        renderItem={({ item }) => <Cesta {...item} produtor={{ nome, imagem }} />}
+        style={estilos.lista}
+    />
+
+}
+
+const estilos = StyleSheet.create({
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    conteudo: {
+        paddingHorizontal: 16,
+    },
+    logo: {
+        flexDirection: 'row',
+    },
+    produtorImage: {
+        width: 62,
+        height: 62,
+        marginTop: -23,
+        borderRadius: 6,
+    },
+    produtor: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginLeft: 16,
+    },
+    cestas: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginTop: 32,
+    }
+});
